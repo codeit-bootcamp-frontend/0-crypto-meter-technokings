c62ca90c0b13758bb8304142ce0348054b8ce03d
SeyoungCho <wnl383@naver.com>
2023-05-12 02:12:25 +0900

    :tada:  보일러 플레이트 추가
    eslintrc, prettierrc, docker-compose, nelify.toml, viteconfig 설정, 파일구조 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+  ],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: {
+    react: { version: "18.2" },
+    "import/resolver": {
+      "eslint-import-resolver-custom-alias": {
+        alias: {
+          "@": "./src",
+          "@components": "./src/components",
+        },
+        extensions: [".js", ".jsx"],
+        packages: ["packages/*"],
+      },
+    },
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    quotes: "off",
+    "no-use-before-define": "off",
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
