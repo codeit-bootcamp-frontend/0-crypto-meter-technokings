300f767745244baea88f20f85cdac9e946c21fef
drizzle96 <dlxkffldk37@naver.com>
2023-05-15 12:22:47 +0900

    :art: @styles alias 추가 및  styles 파일 export 구조 수정

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,10 @@ export default defineConfig({
         find: "@components",
         replacement: path.resolve(__dirname, "src/components"),
       },
+      {
+        find: "@styles",
+        replacement: path.resolve(__dirname, "src/styles"),
+      },
     ],
   },
 });
