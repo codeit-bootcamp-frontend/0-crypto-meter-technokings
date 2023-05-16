d97e14ab262c42b438132174e8e4f95f74fe7274
drizzle96 <dlxkffldk37@naver.com>
2023-05-16 11:29:44 +0900

    :recycle: App 컴포넌트  arrow 함수로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
 import React from "react";
-import "./App.css";
-import SearchHistoryButton from "./components/SearchHistoryButton/SearchHistoryButton";
-import IncreaseMoneyButton from "./components/IncreaseMoneyButton/IncreaseMoneyButton";
-import PriceChange from "./components/PriceChange/PriceChange";
-import SearchOptionInput from "./components/SearchOptionInput/SearchOptionInput";
-import SelectCurrencyButton from "./components/SelectCurrencyButton/SelectCurrencyButton";
-import MainLogo from "./components/SVGComponents/MainLogo";
 
-function App() {
-  return (
-    <>
-      CryptoMeter
-      <div>
-        <SearchHistoryButton />
-      </div>
-      <div>
-        <IncreaseMoneyButton />
-      </div>
-      <div>
-        <PriceChange />
-      </div>
-      <div>
-        <SearchOptionInput />
-      </div>
-      <div>
-        <SelectCurrencyButton />
-      </div>
-      <div>
-        <MainLogo />
-      </div>
-    </>
-  );
-}
+import "./App.css";
+
+const App = () => {
+  return <>CryptoMeter</>;
+};
 
 export default App;
