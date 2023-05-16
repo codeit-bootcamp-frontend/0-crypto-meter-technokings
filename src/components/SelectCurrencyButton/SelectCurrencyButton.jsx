1d0e937b208682f65286ab514bc723fba309b5f3
drizzle96 <dlxkffldk37@naver.com>
2023-05-16 10:43:01 +0900

    :fire: Input, modal 관련 컴포넌트, 코드 삭제
    이유: 케니에게 업무 분담

--- src/components/SelectCurrencyButton/SelectCurrencyButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import SelectCurrencyButtonContainer from "./SelectCurrencyButtonContainer";
-
-const SelectCurrencyButton = () => {
-  return <SelectCurrencyButtonContainer />;
-};
-
-export default SelectCurrencyButton;
