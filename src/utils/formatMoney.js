d2dba93668b29e5a449b33370e7f9c58c2fddd3d
SeyoungCho <wnl383@naver.com>
2023-05-19 11:23:31 +0900

    :sparkles: FormatDateToString 유틸리티 함수 추기
    JSDoc 추가, 기존 formatMoneyToString JSDoc수정

--- src/utils/formatMoney.js
+++ src/utils/formatMoney.js
@@ -1,6 +1,7 @@
 /**
- * @param {*} money: 돈의 액수(number)
- * @returns 화면에 보이는 금액 형식의 스트링으로 변환하여 리턴
+ * @param {Number} money - 돈의 액수
+ * @param {string} unit - 화폐 단위, "krw", "usd" 의 string 형식
+ * @returns {string} 화면에 보이는 금액 형식의 스트링으로 변환하여 리턴
  */
 const formatMoneyToString = (money, unit) => {
   const formattedMoney = new Intl.NumberFormat().format(money);
