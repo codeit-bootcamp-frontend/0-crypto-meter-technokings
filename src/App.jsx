cdc77968ffc32bb5a2e636fca1342059e90a21ef
SeyoungCho <wnl383@naver.com>
2023-05-21 12:35:38 +0000

    :poop: App.jsx에 테스트용 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 
 import "./App.css";
-import useUserInputStore from "@/stores/userInputStore";
 import { Gnb } from "@components";
 import CoinChart from "@components/CoinChart/CoinChart";
 import InputBoard from "@components/InputBoard/InputBoard";
 
 const App = () => {
-  const { calculateMoney, calculatedMoney } = useUserInputStore();
   return (
     <>
       <Gnb />
       <InputBoard />
       <CoinChart />
-      <button onClick={calculateMoney} type="button">
-        계산하기
-      </button>
-      <div>{calculatedMoney}</div>
     </>
   );
 };
