ba0e68572919581630a3b01e7dce1361d0883e0e
SeyoungCho <wnl383@naver.com>
2023-05-21 06:06:52 +0000

    :sparkles: FormatDate유틸 함수에 isQuery 옵셔널 파라미터 추가
    파라미터로 들어온 옵션에 따라 'dd-MM-yyyy'형식으로 포맷하는 기능 추가

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,11 +1,14 @@
 /**
  * @param {Date} date
- * @returns Date 객체를 받아 "yyyy년 MM월 dd일" 이라는 string으로 포맷하여 반환
+ * @param {boolean} isQuery "dd-MM-yyyy"의 쿼리스트링에 이용할 형식으로 포맷하게 해주는 옵션
+ * @returns Date 객체를 받아 "yyyy년 MM월 dd일" 또는 "dd-MM-yyyy" 이라는 string으로 포맷하여 반환
  */
-const formatDateToString = (date) => {
-  return `${date.getFullYear()}년 ${
-    date.getMonth() + 1
-  }월 ${date.getDate()}일 `;
+const formatDateToString = (date, isQuery = false) => {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return isQuery ? `${day}-${month}-${year}` : `${year}년 ${month}월 ${day}일`;
 };
 
 export default formatDateToString;
