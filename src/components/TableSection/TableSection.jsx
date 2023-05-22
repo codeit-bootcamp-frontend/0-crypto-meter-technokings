b6a378e9430bdbfc22b310b56dd83e8b637c1889
wooleejaan <wooleejaan@gmail.com>
2023-05-22 14:54:47 +0900

    :art: 레이아웃 관련 테이블 UI 변경

--- src/components/TableSection/TableSection.jsx
+++ src/components/TableSection/TableSection.jsx
@@ -8,15 +8,7 @@ import Table from "./Table/Table";
 
 const TableSection = () => {
   return (
-    <S.TableSectionWrapper
-      col
-      g={32}
-      br={25}
-      mgl={24}
-      mgr={36}
-      pd="36px 48px 60px 48px"
-      white
-    >
+    <S.TableSectionWrapper col g={32} br={25} pd="36px 48px 60px 48px" white>
       <SHeading3>전체 암호화폐 시세</SHeading3>
       <Table />
     </S.TableSectionWrapper>
@@ -25,14 +17,12 @@ const TableSection = () => {
 
 const S = {};
 
-// Tablet 부터는 페이지 전체 좌우 마진 줄 것으로 보여 마진 0으로 변경함
 S.TableSectionWrapper = styled(SDiv)`
+  width: 100%;
   max-width: 1379px;
 
   @media only screen and (max-width: 1200px) {
-    border-radius: 0;
     margin: 0;
-    padding: 0;
     background-color: ${colors.white};
   }
 
