775e393fefe251fa6c1a3e50d7f3e2f44af22cc4
SeyoungCho <wnl383@naver.com>
2023-05-22 05:38:54 +0000

    :recycle: CoinStore 액션 대폭 축소
    getPage 액션 삭제 및 액션 이름 변경

--- src/stores/coinStore.js
+++ src/stores/coinStore.js
@@ -20,6 +20,7 @@ const mockAxiosMarkets = (currency, coinsPerPage, pageNum, order) => {
  * @param {string} currency 선택한 화폐 단위
  * @returns axios get 요청으로 가져온 180개 코인 리스트 반환
  */
+// eslint-disable-next-line no-unused-vars
 const getCoinsByPage = async (pageNum, coinsPerPage, currency) => {
   const response = await mockAxiosMarkets(
     currency,
@@ -37,89 +38,26 @@ const coinStore = (set, get) => ({
    * @param {string} currency
    * @returns {boolean} 인자로 받은 페이지네이션 넘버에 해당하는 페이지네이션이 coinDB에 있는지 여부를 반환
    */
-  checkCache: (pagiNationNum, currency) => {
-    if (pagiNationNum === 0) return true;
+  checkCache: (pageNum, currency) => {
+    if (pageNum === 0) return true;
     const { coinDB } = get();
-    if (`${currency}_${pagiNationNum}` in coinDB) return true;
+    if (`${currency}_${pageNum}` in coinDB) return true;
     return false;
   },
   /**
    * @param {coinObject[]} coinList 180개 코인 객체가 담긴 배열
-   * @param {number} paginationNum 해당 배열이 몇 번째 페이지에 저장될 지
+   * @param {number} pageNum coinDB에 저장할 페이지(180개 단위) 번호
    * @param {string} currency 화폐 단위
    * @description 파라미터로 받은 정보를 coinDB에 저장
+   * @example addPage(newCoinListFromApiCall, 3, selectedCurrency) -> "krw_3" : [...] 으로 coinDb에 저장
    */
-  saveDataInCoinDB: (coinList, paginationNum, currency) => {
+  addPage: (coinList, pageNum, currency) => {
     set(
       produce((draft) => {
         // eslint-disable-next-line no-param-reassign
-        draft.coinDB[`${currency}_${paginationNum}`] = coinList;
+        draft.coinDB[`${currency}_${pageNum}`] = coinList;
       })
     );
-  },
-  /**
-   * @param {number} pageNum 테이블에 있는 페이지 번호 (30개가 한 페이지)
-   * @param {string} currency 선택한 화폐 단위
-   * @returns {coinObject[]} 파라미터로 받은 페이지 번호를 토대로 coinDB에서 30개를 추출한 배열 반환
-   */
-  getCoinListByPageNum: (pageNum, currency) => {
-    const { coinDB } = get();
-    const paginationNum = Math.ceil(
-      pageNum / import.meta.env.VITE_PAGES_PER_PAGINATION
-    );
-    const startIdx =
-      ((pageNum - 1) % import.meta.env.VITE_PAGES_PER_PAGINATION) *
-      import.meta.env.VITE_COINS_PER_PAGE;
-    const coinList = coinDB[`${currency}_${paginationNum}`].slice(
-      startIdx,
-      startIdx + import.meta.env.VITE_COINS_PER_PAGE
-    );
-    return coinList;
-  },
-  /**
-   * @param {number} pageNum 테이블에 있는 페이지 번호 (30개가 한 페이지)
-   * @param {string} currency 선택한 화폐 단위
-   * @description 요청한 페이지가 coinDB에 있는지 확인 후 없으면 api요청을 통해 coinDB를 갱신 후 요청한 페이지를 반환
-   */
-  getPage: async (pageNum, currency) => {
-    const { checkCache, saveDataInCoinDB, getCoinListByPageNum } = get();
-    // 캐시 체킹 대상 페이지네이션 넘버 2개 추출
-    const pagesPerPagination = import.meta.env.VITE_PAGES_PER_PAGINATION;
-    // 6, 12와 같이 6과 나누어 떨어지는 숫자들은 다르게 처리
-    const currentPaginationNum =
-      pageNum % pagesPerPagination !== 0
-        ? Math.ceil(pageNum / pagesPerPagination)
-        : pageNum / pagesPerPagination;
-    const previousPaginationNum =
-      pageNum % pagesPerPagination !== 0
-        ? Math.floor(pageNum / pagesPerPagination)
-        : pageNum / pagesPerPagination - 1;
-    // 캐시 미스가 나면 api요청을 통해 없는 페이지네이션을 가져와서 coinDB에 저장
-    if (!checkCache(previousPaginationNum, currency)) {
-      try {
-        const newPagination = await getCoinsByPage(
-          previousPaginationNum,
-          import.meta.env.VITE_COINS_PER_PAGE,
-          currency
-        );
-        saveDataInCoinDB(newPagination, previousPaginationNum, currency);
-      } catch (err) {
-        throw new Error(err);
-      }
-    }
-    if (!checkCache(currentPaginationNum, currency)) {
-      try {
-        const newPagination = await getCoinsByPage(
-          currentPaginationNum,
-          import.meta.env.VITE_COINS_PER_PAGE,
-          currency
-        );
-        saveDataInCoinDB(newPagination, currentPaginationNum, currency);
-      } catch (err) {
-        throw new Error(err);
-      }
-    }
-    return getCoinListByPageNum(pageNum, currency);
   },
 });
 
