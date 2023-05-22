b42a0d5943345b3ff4175eda8bc4125bbec6311b
wooleejaan <wooleejaan@gmail.com>
2023-05-22 15:15:58 +0900

    :recycle: 데이터 없을 때 보여줄 default 컴포넌트 분리

--- src/components/CoinChart/CoinChartPresenter.jsx
+++ src/components/CoinChart/CoinChartPresenter.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 import styled from "styled-components";
 
-import { h3 } from "@/styles/text.style";
-import { colors, SDiv, SText } from "@styles";
+import { SDiv } from "@styles";
 
 import CoinChartArea from "./CoinChartArea";
 import CoinChartHeader from "./CoinChartHeader";
 import CoinHistoryUserSelect from "./CoinHistoryUserSelect";
+import DefaultChartImage from "./DefaultChartImage";
 
 const CoinChartPresenter = ({
   coinName,
@@ -25,30 +25,7 @@ const CoinChartPresenter = ({
   onClickChip,
 }) => {
   if (Object.keys(chartData).length === 0) {
-    return (
-      <S.ChartWrapper flex full br={25} white>
-        <S.Inner pos="relative" mg="36px 48px 70px" maxW={910}>
-          <S.NullTextWrapper>
-            <SDiv />
-            입력한 코인 정보가 없습니다.
-            <SDiv row>
-              <S.NullText h3>보드에서&nbsp;</S.NullText>
-              <S.NullText h3 green>
-                코인 정보
-              </S.NullText>
-              <S.NullText h3>를 입력해보세요.</S.NullText>
-            </SDiv>
-          </S.NullTextWrapper>
-          <SDiv full pd="80px">
-            <img
-              src="/null_chart.png"
-              alt="null chart"
-              style={{ maxWidth: "100%", height: "auto", display: "block" }}
-            />
-          </SDiv>
-        </S.Inner>
-      </S.ChartWrapper>
-    );
+    return <DefaultChartImage />;
   }
   return (
     <S.ChartWrapper col full br={25} white>
@@ -91,18 +68,5 @@ S.Inner = styled(SDiv)`
     margin: 32px 20px 0px;
   }
 `;
-S.NullTextWrapper = styled(SDiv)`
-  ${h3}
-  position: absolute;
-  top: 36px;
-  left: -12px;
-  color: ${colors.gray6};
-
-  @media only screen and (max-width: 768px) {
-    left: 16px;
-  }
-`;
-
-S.NullText = styled(SText)``;
 
 export default CoinChartPresenter;
