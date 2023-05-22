ed8ff6b6bcc25852b197294e1b3c89e1a9153341
wooleejaan <wooleejaan@gmail.com>
2023-05-22 15:02:32 +0900

    :art: 메인 페이지 레이아웃 완성

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,56 @@ import React from "react";
 
 import "./App.css";
 
+import styled from "styled-components";
+
 import { Gnb, CoinChart, InputBoard, TableSection } from "@components";
+import { SDiv } from "@styles";
 
 const App = () => {
   return (
-    <>
+    <S.AppWrapper>
       <Gnb />
-      <InputBoard />
-      <CoinChart />
-      <TableSection />
-    </>
+      <S.Contents>
+        <S.InputWrapper>
+          <InputBoard />
+        </S.InputWrapper>
+        <S.ContentsWrapper col g={24}>
+          <CoinChart />
+          <TableSection />
+        </S.ContentsWrapper>
+      </S.Contents>
+    </S.AppWrapper>
   );
 };
+
+const S = {};
+
+S.AppWrapper = styled(SDiv)`
+  width: 100%;
+  max-width: calc(1379px + 445px + 24px);
+  margin: 0 auto 46px;
+`;
+
+S.Contents = styled(SDiv)`
+  display: flex;
+  align-items: start;
+  gap: 24px;
+
+  @media only screen and (max-width: 1200px) {
+    align-items: stretch;
+    flex-direction: column;
+    margin-top: 10px;
+  }
+`;
+
+S.InputWrapper = styled(SDiv)`
+  position: sticky;
+  top: 100px;
+  z-index: 30px;
+`;
+
+S.ContentsWrapper = styled(SDiv)`
+  min-width: 0;
+`;
 
 export default App;
