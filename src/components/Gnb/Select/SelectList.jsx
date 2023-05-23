23a58cea2483b7058926aeb76018af3c451c8b6a
SeyoungCho <wnl383@naver.com>
2023-05-23 13:41:43 +0900

    :sparkles: SelectList 바깥 영역 클릭 시 select 닫히는 기능 추가

--- src/components/Gnb/Select/SelectList.jsx
+++ src/components/Gnb/Select/SelectList.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/function-component-definition */
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
-import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import colors from "@/styles/colors";
@@ -11,8 +10,24 @@ import SDiv from "@styles/micro-components/StyledDiv";
 import SelectListItem from "./SelectListItem";
 
 const SelectList = ({ options, onSelect, onClick, selectedIdx }) => {
+  const selectListRef = useRef(null);
+  const handleClick = (e) => {
+    if (selectListRef.current && !selectListRef.current.contains(e.target)) {
+      onClick();
+    }
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      document.addEventListener("click", handleClick);
+    }, 0);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  });
   return (
     <S.SelectListWrapper
+      ref={selectListRef}
       col
       ast
       pd="4px"
@@ -33,16 +48,6 @@ const SelectList = ({ options, onSelect, onClick, selectedIdx }) => {
       ))}
     </S.SelectListWrapper>
   );
-};
-
-SelectList.propTypes = {
-  options: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-      symbol: PropTypes.string,
-      value: PropTypes.string,
-    })
-  ).isRequired,
 };
 
 const S = {};
