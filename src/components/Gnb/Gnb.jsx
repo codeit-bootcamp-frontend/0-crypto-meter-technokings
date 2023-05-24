41c9d0bec2a3f6f6e179641bad7a7efd3ea59fbb
SeyoungCho <wnl383@naver.com>
2023-05-24 16:13:41 +0900

    :sparkles: 화폐단위 변경 시 사용자 입력금액 초기화

--- src/components/Gnb/Gnb.jsx
+++ src/components/Gnb/Gnb.jsx
@@ -6,15 +6,30 @@ import GnbPresenter from "./GnbPresenter";
 
 const Gnb = () => {
   const [isHistoryOpen, setIsHistoryOpen] = useState(false);
-  const { selectedCurrency, setSelectedCurrency, resetAll } = useUserInputStore(
-    (state) => ({
-      selectedCurrency: state.selectedCurrency,
-      setSelectedCurrency: state.setSelectedCurrency,
-      resetAll: state.resetAll,
-    })
-  );
+  const {
+    selectedCurrency,
+    setSelectedCurrency,
+    resetAll,
+    setSelectedMoney,
+    setCalculatedMoney,
+    calculatedMoney,
+  } = useUserInputStore((state) => ({
+    selectedCurrency: state.selectedCurrency,
+    setSelectedCurrency: state.setSelectedCurrency,
+    resetAll: state.resetAll,
+    setSelectedMoney: state.setSelectedMoney,
+    setCalculatedMoney: state.setCalculatedMoney,
+    calculatedMoney: state.calculatedMoney,
+  }));
   const handleHistoryClick = () => {
     setIsHistoryOpen((prev) => !prev);
+  };
+  const handleSelectCurrency = (newCurrency) => {
+    setSelectedCurrency(newCurrency);
+    setSelectedMoney(0);
+    if (calculatedMoney !== -1) {
+      setCalculatedMoney(0);
+    }
   };
 
   return (
@@ -23,7 +38,7 @@ const Gnb = () => {
       onResetClick={resetAll}
       onHistoryClick={handleHistoryClick}
       selectedCurrency={selectedCurrency}
-      onSelectOption={setSelectedCurrency}
+      onSelectOption={handleSelectCurrency}
     />
   );
 };
